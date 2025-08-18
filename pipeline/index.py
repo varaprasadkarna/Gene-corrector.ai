432bffe1478131cc26aae643642405f27d1bf03c
Varaprasad <varaprasadkarna305@gmail.com>
2025-08-18 21:05:55 +0530

    chaneg import main

--- pipeline/index.py
+++ pipeline/index.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-import main as pipeline # Import main.py and give it an alias 'pipeline'
+from pipeline import main as pipeline
 import sys
 
 # --- Create the Flask App ---
